d11eec9798f1c040e12f7f617b320f0602817159
Anish Antony <anish.antony@nibodha.com>
2015-01-10 20:39:12 +0530

    chnge

--- src/main/java/com/nibodha/lgaas/controller/LightActionController.java
+++ src/main/java/com/nibodha/lgaas/controller/LightActionController.java
@@ -1,11 +1,25 @@
 package com.nibodha.lgaas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nibodha.lgaas.service.LightActionService;
 
 @RestController
 @RequestMapping("/control")
 public class LightActionController {
+	@Autowired
+	LightActionService lightActionService;
+	
+	@RequestMapping("/blink/{level}")
+	public String startLight(@PathVariable("level") Long level){
+		
+		
+		
+		
+	}
 	
 
 }
